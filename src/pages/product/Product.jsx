d59563a0e7b4f70f5041352e9f201f9f904221eb
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-03-13 17:38:44 +0100

    home page and links

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import image from "../../images/set-of-3.jpg"
 
 export default function Product() {
     return (
         <div>
             product page
         </div>
+
     )
 }
