c898da048e4a7e90c4b4645470f102b701887d3c
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-03-17 22:05:45 +0100

    new sections to product page

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NavLink, Outlet } from "react-router-dom";
+import { Link, NavLink, Outlet } from "react-router-dom";
 import image_chopping from "../../images/knife-chopping-2.jpg"
 import { productsData } from "../../data/data"
 
@@ -19,7 +19,6 @@ export default function Product() {
         color: "#24242C",
         textDecoration: "underline"
     }
-
     
     function changeVariant(id) {
         setAllVariants(prev => {            
@@ -53,36 +52,67 @@ export default function Product() {
 
     return (
         <div className="product-page-container">
-            <img src={image} className="product-main-image"/>
-            <h1 className="product-title">Katana knives - {productsIds.length}pcs</h1>
-            <p className="price">${price}</p>
-            <div className="options-container">
-                {variantsButtons}
-            </div>
-            <button className="buy-button">Buy now</button>
-            <nav>
-                <NavLink 
-                    to="details" 
-                    style={({isActive}) => isActive ? active : null}
-                >
-                    Details
-                </NavLink>
-                <NavLink 
-                    to="shipping-info" 
-                    style={({isActive}) => isActive ? active : null}
-                >
-                    Shipping
-                </NavLink>
-                <NavLink 
-                    to="specification" 
-                    style={({isActive}) => isActive ? active : null}
-                >
-                    Specification
-                </NavLink>
-            </nav>
-            <Outlet context={allVariants}/>
+            <section className="product-first-section">
+                <img src={image} className="product-main-image"/>
+                <h1 className="product-title">Katana knives - {productsIds.length}pcs</h1>
+                <p className="price">${price}</p>
+                <div className="options-container">
+                    {variantsButtons}
+                </div>
+                <button className="buy-button">Buy now</button>
+                <nav>
+                    <NavLink 
+                        to="details" 
+                        style={({isActive}) => isActive ? active : null}
+                    >
+                        Details
+                    </NavLink>
+                    <NavLink 
+                        to="shipping-info" 
+                        style={({isActive}) => isActive ? active : null}
+                    >
+                        Shipping
+                    </NavLink>
+                    <NavLink 
+                        to="specification" 
+                        style={({isActive}) => isActive ? active : null}
+                    >
+                        Specification
+                    </NavLink>
+                </nav>
+                <Outlet context={allVariants}/>
+            </section>
             <section>
                 <img src={image_chopping} className="product-description-image"/>
+                <h2>Solid construction</h2>
+                <p>Our knives have solid construction tested many times in hard conditions. They passed every test and now they are ready to be used in your kitchen too!</p>
+            </section>
+            <section>
+                <img src={image_chopping} className="product-description-image"/>
+                <h2>Solid construction</h2>
+                <p>Our knives have solid construction tested many times in hard conditions. They passed every test and now they are ready to be used in your kitchen too!</p>
+            </section>
+            <section className="offer-container">
+                <span><i class="fa-solid fa-chevron-left"></i></span>
+                <div>
+                    <div className="offer-presentation">
+                        <img src={allVariants.sets[0].image}/>
+                        <div className="offer-presentation-info">                        
+                            <Link to=".">
+                            <ul>
+                                <li>Chef's knife</li>
+                                <li>Naikiri knife</li>
+                                <li>Fruit knife</li>
+                                <li>5 day shipping</li>
+                                <li>Free returns</li>
+                            </ul>
+                            <p className="offer-presentation-cta">Buy now</p>
+                            </Link>
+                        </div>
+                    </div>
+                    <p className="offer-presentation-price">${allVariants.sets[0].price}</p>
+                </div>
+                <span><i class="fa-solid fa-chevron-right"></i></span>
             </section>
         </div>
 
