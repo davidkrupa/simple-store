5a60625c4efcb87c38bb4d9fef1e5bb221d4ba1c
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-03-18 23:25:16 +0100

    contact page form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,27 @@
 import React from "react";
 
 export default function Contact() {
+
     return (
-        <h1>Contact element</h1>
+        <div className="contact-form-container">
+            <h1>Contact us</h1>
+            <form className="contact-form">
+                <div>
+                    <input
+                        type="name"
+                        name="name"
+                        placeholder="Name"
+                    />
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="Email address"
+                    />
+                </div>
+                <textarea name="question" rows="4">
+                </textarea>
+                <button>Send your message</button>
+            </form>
+        </div>
     )
 }
