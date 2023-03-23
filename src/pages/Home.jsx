376277adde4924503b8a7013710c8c8934bf0df0
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-03-23 19:48:00 +0100

    home responsive improvement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,6 +19,10 @@ export default function Home() {
     const featureVisible = {
         display: "block"
     }
+    
+    const featureNotVisible = {
+        display: "none",
+    }
 
     console.log(isFeatureActive)
 
@@ -28,35 +32,77 @@ export default function Home() {
                 <h1>Katana Knives</h1>
                 <h3>Traditional Japanesee Steel</h3>
             </div>
+            
             <section className="home-paragraph">                
                 <h2>Sharpened the same techique as legendary katana sword</h2>
                 <p>At the heart of Japanese kitchen knives is the concept of "sharpening." Japanese bladesmiths believe that sharpening is an art form that requires patience, precision, and a deep understanding of the materials being used. Whether you're a sushi chef or a home cook our Japanese knives will elevate your culinary game and make every meal a work of art.</p>
                 <Link to='knives'>
                     <button className="paragraph-button">Find out more</button>
                 </Link>
-
             </section>
+
             <section className="features-container">
                 <img src={chopping}></img>
-                <div className="features-responsive">
-                    <div className="feature-1" onClick={() => showFeature("feature1")}>
-                        <h3>Razor Sharp</h3>
-                        <p style={isFeatureActive[0].isActive ? featureVisible : null}>The sharpness of the blade allows for clean, precise cuts that preserve the integrity of the food being prepared. Great precision and control when cutting.</p>
+                <div className="features-table">
+                    <div 
+                        className="feature"
+                        onClick={() => showFeature("feature1")}
+                    >
+                        <h3>Razor Sharp<span>+</span></h3>
+                        <p 
+                            style={isFeatureActive[0].isActive 
+                                ? {display: "block"} 
+                                : {display: "none"}
+                            }
+                        >
+                            The sharpness of the blade allows for clean, precise cuts that preserve the integrity of the food being prepared. Great precision and control when cutting.
+                        </p>
                     </div>
-                    <div className="feature-2" onClick={() => showFeature("feature2")}>
-                        <h3>Comfortable</h3>
-                        <p style={isFeatureActive[1].isActive ? featureVisible : null}>Our knives handles provide a comfortable grip and help prevent hand fatigue during extended use.</p>
+                    <div 
+                        className="feature" 
+                        onClick={() => showFeature("feature2")}
+                    >
+                        <h3>Comfortable<span>+</span></h3>
+                        <p 
+                            style={isFeatureActive[1].isActive 
+                                ? {display: "block"} 
+                                : {display: "none"}
+                            }
+                        >
+                            Our knives handles provide a comfortable grip and help prevent hand fatigue during extended use.
+                        </p>
                     </div>
-                    <div className="feature-3" onClick={() => showFeature("feature3")}>
-                        <h3>Handcrafted</h3>
-                        <p style={isFeatureActive[2].isActive ? featureVisible : null}>Handcrafted using traditional techniques. Each knife is unique and reflects the skill and artistry of the bladesmith</p>
+                    <div 
+                        className="feature" 
+                        onClick={() => showFeature("feature3")}
+                    >
+                        <h3>Handcrafted<span>+</span></h3>
+                        <p 
+                            style={isFeatureActive[2].isActive 
+                                ? {display: "block"} 
+                                : {display: "none"}
+                            }
+                        >
+                            Handcrafted using traditional techniques. Each knife is unique and reflects the skill and artistry of the bladesmith.
+                        </p>
                     </div>
-                    <div className="feature-4" onClick={() => showFeature("feature4")}>
-                        <h3>Durable</h3>
-                        <p style={isFeatureActive[3].isActive ? featureVisible : null}>Made of hard, high-carbon steel that lasts longer than other types of steel. This steel is also more resistant to corrosion, making it an ideal material for kitchen knives.</p>
+                    <div 
+                        className="feature" 
+                        onClick={() => showFeature("feature4")}
+                    >
+                        <h3>Durable<span>+</span></h3>
+                        <p 
+                            style={isFeatureActive[3].isActive 
+                                ? {display: "block"} 
+                                : {display: "none"}
+                            }
+                        >
+                            Made of hard, high-carbon steel that lasts longer than other types of steel. This steel is more resistant to corrosion, making it an ideal material for kitchen knives.
+                        </p>
                     </div>
                 </div>
             </section>
+
             <section className="home-paragraph-second">
                 <h2>Forged for every kitchen <span>Samurai</span></h2>
                 <p>Katana knives are more than just a tool. They are a symbol of Japanese craftsmanship, tradition, and culture. With their sharp edges, elegant designs, and exceptional quality, these knives are a testament to the artistry and dedication of the bladesmiths who create them.</p>
