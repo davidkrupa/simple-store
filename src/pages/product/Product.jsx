af06a86da2dc890b736e9982d94842fff57563dc
davidkrupa <126403373+davidkrupa@users.noreply.github.com>
2023-03-27 21:39:36 +0200

    reduced size of some non-significant images

--- src/pages/product/Product.jsx
+++ src/pages/product/Product.jsx
@@ -4,6 +4,7 @@ import image_chopping from "../../images/knife-chopping-2.jpg"
 import review1 from "../../images/review1.jpg"
 import review2 from "../../images/review2.jpg"
 import review3 from "../../images/review3.jpg"
+import stars from "../../images/stars.png"
 import { productsData } from "../../data/data"
 
 export default function Product() {    
@@ -66,15 +67,13 @@ export default function Product() {
             
         })
         return (
-            <div className="offer-presentation" key={set.id}>
-                <img src={set.image}/>
-                <div className="offer-presentation-info" onClick={() => handleBuyClick()}>                        
-                    <ul>
-                        {knifeNames}
-                    </ul>
-                    <p className="offer-presentation-price">${set.price}</p>
-                    <p className="offer-presentation-cta">Buy now</p>
-                </div>
+            <div className="offer-presentation" key={set.id} onClick={() => handleBuyClick()}>
+                <img src={set.image}/>                   
+                <ul>
+                    {knifeNames}
+                </ul>
+                <p className="offer-presentation-price">${set.price}</p>
+                <p className="offer-presentation-cta">Buy now</p>
             </div>
         )
     })
@@ -121,7 +120,7 @@ export default function Product() {
 
             <section className="description-section">
                 <img src={image_chopping} className="product-description-image"/>
-                <div className="description-background-box">
+                <div className="description-box">
                     <h2>Solid construction</h2>
                     <p>Japanese kitchen knives are known for their solid construction, which is achieved through the use of high-quality materials and traditional forging techniques. The blades are made of high-carbon or steel, and are carefully crafted to ensure sharpness, durability, and precision cutting.</p>
                 </div>
@@ -129,7 +128,7 @@ export default function Product() {
 
             <section className="description-section reverse">
                 <img src={image_chopping} className="product-description-image"/>
-                <div className="description-background-box reverse-box">
+                <div className="description-box reverse-box">
                     <h2>For chefs and home cooks</h2>
                     <p>We take pride in offering high-quality kitchen knives that are both functional and stylish. Katana knives allow for more precise slicing, dicing, and chopping. Our knives are perfect for both professional chefs and home cooks who are looking for reliable and long-lasting tools in the kitchen.</p>
                 </div>
@@ -137,9 +136,9 @@ export default function Product() {
 
             <section className="description-section">
                 <img src={image_chopping} className="product-description-image"/>
-                <div className="description-background-box">
+                <div className="description-box">
                     <h2>Ergonomic design</h2>
-                    <p>Ergonomic design is a key feature of Katana kitchen knives, with the handles and blades designed to fit comfortably in the user's hand. The balance of the knife is carefully considered, with the weight distributed evenly along the blade and handle. This design ensures that the knife feels balanced and stable</p>
+                    <p>Ergonomic design is a key feature of Katana kitchen knives, with the handles and blades designed to fit comfortably in the user's hand. The balance of the knife is carefully considered, with the weight distributed evenly along the blade and handle. This design ensures that the knife feels balanced and stable.</p>
                 </div>
             </section>
 
@@ -154,22 +153,31 @@ export default function Product() {
                 <h2>Reviews</h2>
                 <div className="review-container">
                     <img src={review1} className="review-image" />
-                    <div>
-                        <h4>Name</h4>
+                    <div className="review-body">
+                        <div className="review-name-container"> 
+                            <h4>Brenda T.</h4>
+                            <img src={stars} className="review-stars" />
+                        </div>
                         <p>Those knives are amazing! I use them every day and my friend always ask where did I get them. I am so haapy to have them. Highly recommended!</p>
                     </div>
                 </div>
                 <div className="review-container">
                     <img src={review2} className="review-image" />
-                    <div>
-                        <h4>Name</h4>
+                    <div className="review-body">
+                        <div className="review-name-container">                        
+                            <h4>Rudolf S.</h4>
+                            <img src={stars} className="review-stars" />
+                        </div>
                         <p>Those knives are amazing! I use them every day and my friend always ask where did I get them. I am so haapy to have them. Highly recommended!</p>
                     </div>
                 </div>
                 <div className="review-container">
                     <img src={review3} className="review-image" />
-                    <div>
-                        <h4>Name</h4>
+                    <div className="review-body">
+                        <div className="review-name-container">
+                            <h4>Merry J.</h4>
+                            <img src={stars} className="review-stars" />
+                        </div>
                         <p>Those knives are amazing! I use them every day and my friend always ask where did I get them. I am so haapy to have them. Highly recommended!</p>
                     </div>
                 </div>
